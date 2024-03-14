71cccd71fd65478f8f330a5629de7546908bc16d
Kamen Stoykov <k.stoykov@razorlabs.com>
2024-03-14 07:39:58 +0000

    improve initial funding

--- src/tx_target/tx_target_funding.go
+++ src/tx_target/tx_target_funding.go
@@ -16,36 +16,47 @@ import (
 )
 
 func (tt *TxTarget) EnsureFunding(txs []*types.Tx) error {
-	var balance *big.Int
+	// var balance *big.Int
 	var fundTx []byte
 	var err error
 	var txHash string
-	fromsMap := map[string]string{}
+	fromsMap := map[string]*uint256.Int{}
 
 	for _, tx := range txs {
-		fromsMap[tx.From] = tx.From
+		fundingSoFar, ok := fromsMap[tx.From]
+		if !ok {
+			fundingSoFar = uint256.NewInt(0)
+		}
+
+		fundingSoFar = fundingSoFar.Add(fundingSoFar, tx.GetFundingAsUint256())
+
+		fromsMap[tx.From] = fundingSoFar
 	}
 
 	fmt.Printf("Checking %d transactions for initial funding\n", len(fromsMap))
 	fundTxs := make([][]byte, 0, len(fromsMap))
 	nonceAddition := uint64(0)
-	for from := range fromsMap {
-		if balance, err = tt.getBalanceByHexAddress(from); err != nil {
+	totalFunding := uint256.NewInt(0)
+	for from, fundingAmount := range fromsMap {
+		// if balance, err = tt.getBalanceByHexAddress(from); err != nil {
+		// 	return err
+		// }
+
+		// if balance.Cmp(big.NewInt(2000000000000000000)) >= 0 {
+		// 	continue
+		// }
+
+		totalFunding = totalFunding.Add(totalFunding, fundingAmount)
+		if fundTx, err = tt.makeFundingTx(from, fundingAmount, nonceAddition); err != nil {
 			return err
 		}
 
-		if balance.Cmp(big.NewInt(2000000000000000000)) >= 0 {
-			continue
-		}
-
-		if fundTx, err = tt.makeFundingTx(from, nonceAddition); err != nil {
-			return err
-		}
 		fundTxs = append(fundTxs, fundTx)
 		nonceAddition++
 	}
+	totalFunding = totalFunding.Div(totalFunding, uint256.NewInt(1000000000000000000))
 
-	fmt.Printf("Broadcasting %d transactions for preparing initial funding\n", len(fundTxs))
+	fmt.Printf("Broadcasting %d transactions for preparing initial funding including ~%d ETH\n", len(fundTxs), totalFunding.Uint64())
 	fundTxsHashes := make([]string, 0, SENDING_QUEUE_SIZE)
 	for i, fundTx := range fundTxs {
 		if txHash, err = tt.SendTx("", fundTx); err != nil {
@@ -74,7 +85,7 @@ func (tt *TxTarget) EnsureFunding(txs []*types.Tx) error {
 	return nil
 }
 
-func (tt *TxTarget) makeFundingTx(recipientAddress string, nonceAddition uint64) ([]byte, error) {
+func (tt *TxTarget) makeFundingTx(recipientAddress string, fundingAmount *uint256.Int, nonceAddition uint64) ([]byte, error) {
 	privateKey, err := crypto.HexToECDSA(tt.faucetPrivateKey)
 	if err != nil {
 		return nil, err
@@ -97,7 +108,7 @@ func (tt *TxTarget) makeFundingTx(recipientAddress string, nonceAddition uint64)
 		CommonTx: ethtypes.CommonTx{
 			Nonce: nonce + nonceAddition,
 			To:    &toAddress,
-			Value: uint256.MustFromBig(big.NewInt(0).Mul(big.NewInt(1000000000000000000), big.NewInt(0).SetUint64(tt.fundingAmount))),
+			Value: fundingAmount,
 			Gas:   uint64(1000000),
 			Data:  nil,
 			V:     *uint256.MustFromBig(big.NewInt(2237)),
--- src/tx_target/types/tx.go
+++ src/tx_target/types/tx.go
@@ -1,11 +1,6 @@
 package types
 
-type Tx struct {
-	Bytes []byte  `json:"bytes"`
-	From  string  `json:"from"`
-	To    *string `json:"to,omitempty"`
-	Hash  string  `json:"hash"`
-}
+import "github.com/holiman/uint256"
 
 type TxType int64
 
@@ -16,11 +11,24 @@ const (
 	DynamicFeeTx TxType = 0x02
 )
 
-func NewTx(bytes []byte, from string, to *string, hash string, status uint64) *Tx {
+type Tx struct {
+	Bytes   []byte  `json:"bytes"`
+	From    string  `json:"from"`
+	To      *string `json:"to,omitempty"`
+	Hash    string  `json:"hash"`
+	Funding string  `json:"funding"`
+}
+
+func NewTx(bytes []byte, from string, to *string, hash string, funding string) *Tx {
 	return &Tx{
-		Bytes: bytes,
-		From:  from,
-		To:    to,
-		Hash:  hash,
+		Bytes:   bytes,
+		From:    from,
+		To:      to,
+		Hash:    hash,
+		Funding: funding,
 	}
+}
+
+func (tx *Tx) GetFundingAsUint256() *uint256.Int {
+	return uint256.MustFromHex(tx.Funding)
 }
